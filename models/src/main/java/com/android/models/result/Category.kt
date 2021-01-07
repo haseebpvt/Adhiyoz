bc52e0e038dc348565b28b8a156a43c901759336
haseebcgt <abdul.haseeb@cgtindia.in>
2021-01-07 13:20:27 +0530

    Added comment in category model

--- models/src/main/java/com/android/models/result/Category.kt
+++ models/src/main/java/com/android/models/result/Category.kt
@@ -4,5 +4,9 @@ data class Category(
     val productId: String,
     val categoryName: String,
     val description: String,
+
+    /**
+     * Represents if the category is active now. Shouldn't show if the category is inactive
+     */
     val active: Boolean
 )
