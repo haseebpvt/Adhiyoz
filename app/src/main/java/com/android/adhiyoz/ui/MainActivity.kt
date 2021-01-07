45fde31c93169fd03a601c1afce3f75efcabbb93
haseebcgt <abdul.haseeb@cgtindia.in>
2021-01-07 15:06:11 +0530

    Setting up hilt

--- app/src/main/java/com/android/adhiyoz/ui/MainActivity.kt
+++ app/src/main/java/com/android/adhiyoz/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.android.adhiyoz
+package com.android.adhiyoz.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -6,8 +6,11 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.android.adhiyoz.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
