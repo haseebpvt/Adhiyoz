0e71c665fbb443884810c66f1719b2547533eaaf
haseebcgt <abdul.haseeb@cgtindia.in>
2021-04-22 14:10:12 +0530

    Loading product details in checkout screen

--- app/src/main/java/com/android/adhiyoz/ui/checkout/CheckoutFragment.kt
+++ app/src/main/java/com/android/adhiyoz/ui/checkout/CheckoutFragment.kt
@@ -5,17 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.android.adhiyoz.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.android.adhiyoz.databinding.FragmentCheckoutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CheckoutFragment : Fragment() {
 
+    private val viewModel: CheckoutViewModel by viewModels()
+
+    private val args: CheckoutFragmentArgs by navArgs()
+
+    private lateinit var binding: FragmentCheckoutBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkout, container, false)
+    ): View {
+        binding = FragmentCheckoutBinding.inflate(inflater, container, false).apply {
+            viewmodel = viewModel
+            lifecycleOwner = this@CheckoutFragment
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.loadProductDetails(productId = args.productId)
     }
 }
--- app/src/main/java/com/android/adhiyoz/ui/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/android/adhiyoz/ui/checkout/CheckoutViewModel.kt
@@ -1,5 +1,35 @@
 package com.android.adhiyoz.ui.checkout
 
+import android.util.Log
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.adhiyoz.domain.product.GetProductDetailsWithProductIdUseCase
+import com.android.adhiyoz.result.Result
+import com.android.models.Product
+import kotlinx.coroutines.launch
 
-class CheckoutViewModel : ViewModel()
+class CheckoutViewModel @ViewModelInject constructor(
+    private val getProductDetailsWithProductIdUseCase: GetProductDetailsWithProductIdUseCase
+) : ViewModel() {
+
+    private val _product = MutableLiveData<Product>()
+    val product: LiveData<Product> = _product
+
+    fun loadProductDetails(productId: String) {
+        viewModelScope.launch {
+            when (val result = getProductDetailsWithProductIdUseCase(productId)) {
+                is Result.Success -> {
+                    _product.value = result.data
+                    Log.d("custom_log", "Success: ${result.data}")
+                }
+
+                is Result.Error -> {
+                    Log.d("custom_log", "Failed: ${result.exception}")
+                }
+            }
+        }
+    }
+}
